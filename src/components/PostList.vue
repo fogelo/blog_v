3a8601cbb1f1bef61436e263c6097e384c63161c
antonorlov <fogelo@yandex.ru>
2022-06-23 22:31:58 +0300

    v-if

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,5 +1,10 @@
 <template>
-  <post-item v-for="post in posts" v-bind:key="post.id" :post="post" @remove="$emit('remove', post)"/>
+  <div v-if="posts.length > 0">
+    <post-item v-for="post in posts" v-bind:key="post.id" :post="post" @remove="$emit('remove', post)"/>
+  </div>
+  <h2 v-else style="color: red">
+    Список постов пуст
+  </h2>
 </template>
 
 <script>
